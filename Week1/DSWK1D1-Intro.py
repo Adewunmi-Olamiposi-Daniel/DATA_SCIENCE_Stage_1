4ddf4a71e1b3c27e80bdd9edeb9c9755db5f976a
Adewunmi-Olamiposi-Daniel <olamiposiadewunmi88@gmail.com>
2025-11-03 21:39:15 +0100

    Added Week 1 Day 1 Code Snippet

---
+++ Week1/DSWK1D1-Intro.py
@@ -0,0 +1,14 @@
+#Practice
+print("My name is Adewunmi Olamiposi Daniel")
+print("Welcome to Python Foundations!")
+
+#Pratice 2
+print("Data Science", "is", "Awesome!")
+
+#Pratice 3
+print("She said,\" Python is fun!\"")
+
+#Challenge
+name=input("WHAT IS YOUR NAME?\n")
+school=input("WHAT SCHOOL DO YOU ATTEND?\n")
+CGPA=input("WHAT IS YOUR CURRENT CGPA?\n")
